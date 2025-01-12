c3226a9f7603364974ce42a94026e47b2e78224e
testuser@gmail.com <testuser@gmail.com>
2025-01-12 10:09:55 +0530

    Changes to TRUE value

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "terraform_test_vpc" {
  
   cidr_block         = "172.31.0.0/16"
-  enable_dns_hostnames = false
-  enable_dns_support   = false
+  enable_dns_hostnames = True
+  enable_dns_support   = True
  
   tags = {
     Name = "terraform_test_vpc"
